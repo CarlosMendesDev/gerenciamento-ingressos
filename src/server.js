517b3abbdec5cf8f0124c409609ea8c072bd21d6
Carlos Mendes <52838023+CarlosMendesDev@users.noreply.github.com>
2022-02-08 14:51:40 -0300

    feat(database): conecta com banco de dados

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
+import 'dotenv/config';
 import server from './app.js';
+import sequelize from './service/DatabaseConnection.js';
 
-const port =  5000;
+sequelize.sync().catch((err) => {
+  console.log(err);
+});
 
-  server.listen(port, () => {
-    console.log(`Start server on port ${port}! :D`);
-  });
+server.listen(process.env.PORT, () => {
+  console.log(`Start server on port ${process.env.PORT}! :D`);
+});
---
+++ src/service/DatabaseConnection.js
@@ -0,0 +1,15 @@
+import Sequelize from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URI,
+{
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+
+export default sequelize;
